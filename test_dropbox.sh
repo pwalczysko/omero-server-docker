2450ea9d7d46fce6a3a7cc2e78f2222d45f7d2f8
pwalczysko <p.walczysko@dundee.ac.uk>
2025-11-19 20:13:37 +0000

    no dropbox

--- test_dropbox.sh
+++ test_dropbox.sh
@@ -17,22 +17,22 @@ echo -n "Checking for imported DropBox image $FILENAME "
 # Retry for 4 mins
 i=0
 result=
-while [ $i -lt 60 ]; do
-    sleep 4
-    result=$(docker exec $PREFIX-server $OMERO hql -q -s $SERVER -u $OMERO_USER -w $OMERO_PASS "SELECT COUNT (*) FROM Image WHERE name='$FILENAME'" --style plain)
-    if [ "$result" = "0,1" ]; then
-        echo
-        echo "Found image: $result"
-        exit 0
-    fi
-    if [ "$result" != "0,0" ]; then
-        echo
-        echo "Unexpected query result: $result"
-        exit 2
-    fi
-    echo -n "."
-    let ++i
-done
+# while [ $i -lt 60 ]; do
+#     sleep 4
+#     result=$(docker exec $PREFIX-server $OMERO hql -q -s $SERVER -u $OMERO_USER -w $OMERO_PASS "SELECT COUNT (*) FROM Image WHERE name='$FILENAME'" --style plain)
+#     if [ "$result" = "0,1" ]; then
+#         echo
+#         echo "Found image: $result"
+#         exit 0
+#     fi
+#     if [ "$result" != "0,0" ]; then
+#         echo
+#         echo "Unexpected query result: $result"
+#         exit 2
+#     fi
+#     echo -n "."
+#     let ++i
+# done
 
 echo "Failed to find image"
 exit 2
